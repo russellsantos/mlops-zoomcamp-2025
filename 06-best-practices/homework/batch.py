b5ad2148b344122f77ac0ffaead97c1b05d09c36
Russell Santos <russellsantos@users.noreply.github.com>
2025-06-29 21:59:19 +0800

    add: answered homework questions for module 06

--- 06-best-practices/homework/batch.py
+++ 06-best-practices/homework/batch.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sys
+import os
 import pickle
 import pandas as pd
 
@@ -17,16 +18,41 @@ def prepare_data(df, categorical):
     return df
 
 def read_data(filename:str, categorical:list):
-    df = pd.read_parquet(filename)
+    s3_endpoint_url = os.getenv('S3_ENDPOINT_URL', None)
+    if s3_endpoint_url is not None:
+        options = {'client_kwargs': {'endpoint_url': os.getenv('S3_ENDPOINT_URL')}}
+        df = pd.read_parquet(path=filename, storage_options=options)
+    else: 
+        df = pd.read_parquet(filename)
     return prepare_data(df, categorical)
+
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = 's3://nyc-duration-prediction-alexey/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
+def save_data(filename:str, df_result):
+    print("Saving to", filename)
+    s3_endpoint_url = os.getenv('S3_ENDPOINT_URL', None)
+    output_file = get_output_path(year, month)  
+    if s3_endpoint_url is not None:
+        options = {'client_kwargs': {'endpoint_url': os.getenv('S3_ENDPOINT_URL')}}
+        df_result.to_parquet(path=filename, engine='pyarrow', index=False, storage_options=options)
+    else:
+        df_result.to_parquet(path=filename, engine='pyarrow', index=False)
 
 def main(year, month):
     year =  year
     month = month 
 
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-
+    input_file = get_input_path(year, month) 
+    output_file = get_output_path(year, month) 
 
     with open('model.bin', 'rb') as f_in:
         dv, lr = pickle.load(f_in)
@@ -44,14 +70,13 @@ def main(year, month):
 
 
     print('predicted mean duration:', y_pred.mean())
-
+    print('predicted sum duration:', y_pred.sum())
 
     df_result = pd.DataFrame()
     df_result['ride_id'] = df['ride_id']
     df_result['predicted_duration'] = y_pred
 
-
-    df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    save_data(output_file, df_result)
 
 
 if __name__ == '__main__':
